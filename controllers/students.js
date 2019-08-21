4d4d35bb65901fea4d119bd40c1551c5f6e6e125
anami823 <anami823@gmail.com>
2019-08-21 10:33:21 -0700

    Add OAuth id , secret, callback to .env

--- controllers/students.js
+++ controllers/students.js
@@ -8,8 +8,7 @@ module.exports = {
 
 function index(req, res, next) {
   console.log(req.query)
-  // Make the query object to use with Student.find based up
-  // the user has submitted the search form or now
+  // Make the query object based on the data that the user submitted in the 'search' box on the page
   let modelQuery = req.query.name ? {name: new RegExp(req.query.name, 'i')} : {};
   // Default to sorting by name
   let sortKey = req.query.sort || 'name';
