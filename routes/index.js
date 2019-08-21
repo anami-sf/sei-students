e0b12fe84dac33465b5f49b2bea7c5c010b27f7e
anami823 <anami823@gmail.com>
2019-08-21 13:35:40 -0700

    Add login/logout routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,29 @@
 var router = require('express').Router();
+var passport = require('passport')
 
 // The root route renders our only view
 router.get('/', function(req, res) {
+  res.redirect('/students');
+});
+
+//Add login Route
+router.get('/auth/google', passport.authenticate(
+  'google',
+  { scope: ['profile', 'email'] }
+));
+
+// Google OAuth callback route
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect : '/students',
+    failureRedirect : '/students'
+  }
+))
+
+// OAuth logout route
+router.get('/logout', function(req, res){
+  req.logout();
   res.redirect('/students');
 });
 
