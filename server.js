6f89716a85ee1e7cf8a43005d1c5a8282b8f94f0
anami823 <anami823@gmail.com>
2019-08-21 13:23:29 -0700

    Config passport

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var session = require('express-session')
+var passport = require('passport')
+
+
 
 // load the env vars
 require('dotenv').config();
@@ -11,6 +15,7 @@ var app = express();
 
 // connect to the MongoDB with mongoose
 require('./config/database');
+require('./config/passport')
 
 // require our routes
 var indexRoutes = require('./routes/index');
@@ -25,6 +30,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(session({
+  secret: 'SEIRocks!',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use( passport.initialize( ) );
+app.use( passport.session( ) );
 
 // mount all routes with appropriate base paths
 app.use('/', indexRoutes);
